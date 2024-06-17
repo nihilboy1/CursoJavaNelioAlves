926fdbf1f9528bf7945d5c18a273f7ddcd743f00
nihilboy1 <samuelseve1@gmail.com>
2024-06-17 19:41:24 -0300

    movings pieces

--- XADREZDESAFIO/src/application/UI.java
+++ XADREZDESAFIO/src/application/UI.java
@@ -1,7 +1,11 @@
 package application;
 
 import chess.ChessPiece;
+import chess.ChessPosition;
 import chess.Color;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class UI{
     // https://stackoverflow.com/questions/5762491/how-to-print-color-in-console-using-system-out-println
@@ -24,6 +28,20 @@ public class UI{
     public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
     public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
     public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
+
+
+    public static ChessPosition readChessPosition(Scanner sc){
+        try{
+            String userInputPosition = sc.nextLine();
+            char column = userInputPosition.charAt(0);
+            int row = Integer.parseInt(userInputPosition.substring(1));
+
+            return new ChessPosition(column, row);
+        }catch(RuntimeException e){
+            throw new InputMismatchException("Error on ChessPosition Reading: Valid values are from a1 to h8");
+
+        }
+    }
 
     public static void printBoard(ChessPiece[][] pieces){
         for(int i = 0; i <pieces.length; i++){
