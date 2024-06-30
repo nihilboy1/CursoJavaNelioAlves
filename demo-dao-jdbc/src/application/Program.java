3e9c18be65d0c437d55ac06ba07622c1d44c3314
nihilboy1 <samuelseve1@gmail.com>
2024-06-30 08:26:08 -0300

    insert implementation

--- demo-dao-jdbc/src/application/Program.java
+++ demo-dao-jdbc/src/application/Program.java
@@ -27,5 +27,20 @@ public class Program{
         for(Seller item : findAllList){
             System.out.println(item);
         }
+
+        System.out.println();
+        System.out.println("=== TESTE N4: seller insert -");
+        // Seller newSeller = new Seller(null, "Emil", "emil@gmail.com", new Date(), 4000.0, new Department(2, null));
+        // sellerDAO.insert(newSeller);
+        // System.out.println(newSeller);
+
+        System.out.println();
+        System.out.println("=== TESTE N5: seller update -");
+        Seller seller1 = sellerDAO.findById(2);
+        System.out.println(seller1);
+        seller1.setName("Clark");
+        seller1.setEmail("clark@gmail.com");
+        sellerDAO.update(seller1);
+        System.out.println(seller1);
     }
 }
--- demo-dao-jdbc/src/model/dao/impl/SellerDAOJDBC.java
+++ demo-dao-jdbc/src/model/dao/impl/SellerDAOJDBC.java
@@ -6,10 +6,7 @@ import model.dao.SellerDAO;
 import model.entities.Department;
 import model.entities.Seller;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -23,13 +20,52 @@ public class SellerDAOJDBC implements SellerDAO{
     }
 
     @Override
-    public void insert(Seller department){
+    public void insert(Seller seller){
+        PreparedStatement prepStatement = null;
+        try{
+            prepStatement = connection.prepareStatement("INSERT INTO seller (Name, Email, BirthDate, BaseSalary, " + "DepartmentId) VALUES (?, ?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
+            prepStatement.setString(1, seller.getName());
+            prepStatement.setString(2, seller.getEmail());
+            prepStatement.setDate(3, new Date(seller.getBirthDate().getTime()));
+            prepStatement.setDouble(4, seller.getBaseSalary());
+            prepStatement.setInt(5, seller.getDepartment().getId());
 
+            int affectedRows = prepStatement.executeUpdate();
+            if(affectedRows > 0){
+                ResultSet resultSet = prepStatement.getGeneratedKeys();
+                if(resultSet.next()){
+                    seller.setId(resultSet.getInt(1));
+                }
+                DB.closeResultSet(resultSet);
+            }else{
+                throw new DbException("Unexpected error: no rows affected.");
+            }
+        }catch(SQLException e){
+            throw new DbException(e.getMessage());
+        }finally{
+            DB.closeStatement(prepStatement);
+        }
     }
 
     @Override
-    public void update(Seller department){
+    public void update(Seller seller){
+        PreparedStatement prepStatement = null;
+        try{
+            prepStatement = connection.prepareStatement("UPDATE seller SET Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ? WHERE Id = ?", Statement.RETURN_GENERATED_KEYS);
 
+            prepStatement.setString(1, seller.getName());
+            prepStatement.setString(2, seller.getEmail());
+            prepStatement.setDate(3, new Date(seller.getBirthDate().getTime()));
+            prepStatement.setDouble(4, seller.getBaseSalary());
+            prepStatement.setInt(5, seller.getDepartment().getId());
+            prepStatement.setInt(6, seller.getId());
+
+            prepStatement.executeUpdate();
+        }catch(SQLException e){
+            throw new DbException(e.getMessage());
+        }finally{
+            DB.closeStatement(prepStatement);
+        }
     }
 
     @Override
